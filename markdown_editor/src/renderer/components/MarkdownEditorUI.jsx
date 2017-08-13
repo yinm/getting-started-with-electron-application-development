1fcde4e30d2d722a041a9a3c7d6cc62d4a62ce58
yinm <yinm.cminor9@gmail.com>
2017-08-13 21:21:19 +0900

    add save text and handle event of change

--- markdown_editor/src/renderer/components/MarkdownEditorUI.jsx
+++ markdown_editor/src/renderer/components/MarkdownEditorUI.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 export default class MarkdownEditorUI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.onChangeText = this.onChangeText.bind(this);
+  }
+
+  onChangeText(e) {
+    this.setState({ text: e.target.value });
+  }
+
   render() {
     return (
       <div>
