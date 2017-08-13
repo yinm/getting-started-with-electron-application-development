de428aa99e8e4304ab010a2b644554e8b82b5133
yinm <yinm.cminor9@gmail.com>
2017-08-13 20:53:51 +0900

    add app menu function

--- markdown_editor/src/main/index.js
+++ markdown_editor/src/main/index.js
@@ -1,10 +1,33 @@
 import { app } from "electron";
 import createMainWindow from "./createMainWindow";
+import setAppMenu from "./setAppMenu";
 
 let mainWindow = null;
 
+function openFile() {
+  console.log('openFile');
+}
+
+function saveFile() {
+  console.log('saveFile');
+}
+
+function saveAsNewFile() {
+  console.log('saveAsNewFile');
+}
+
+function exportPDF() {
+  console.log('exportPDF');
+}
+
 app.on('ready', () => {
   mainWindow = createMainWindow();
+  setAppMenu({
+    openFile,
+    saveFile,
+    saveAsNewFile,
+    exportPDF
+  });
 });
 
 app.on('window-all-closed', () => {
