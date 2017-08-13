150e1f5c452c7280a1b114a8f5ad78ff2e5bd9a0
yinm <yinm.cminor9@gmail.com>
2017-08-13 21:08:30 +0900

    add Markdown components

--- markdown_editor/src/renderer/app.jsx
+++ markdown_editor/src/renderer/app.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
+import MarkdownEditorUI from "./components/MarkdownEditorUI";
 
 render(
-  <div>Hello, Electron and React JSX</div>,
+  <MarkdownEditorUI />,
   document.getElementById('app')
 );
---
+++ markdown_editor/src/renderer/components/MarkdownEditorUI.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MarkdownEditorUI extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, MarkdownEditor!</h1>
+      </div>
+    );
+  }
+}
