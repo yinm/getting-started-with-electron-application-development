e6014348c138687a33da74128827f006bd52de06
yinm <yinm.cminor9@gmail.com>
2017-08-13 21:38:24 +0900

    add editor style

--- markdown_editor/src/renderer/components/Editor.jsx
+++ markdown_editor/src/renderer/components/Editor.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import style from "./Editor.css";
 
 export default function Editor(props) {
   return (
     <textareaa
       id="editor"
+      className={`${style.editor} ${props.className}`}
       value={props.value}
       onChange={props.onChange}
     />
