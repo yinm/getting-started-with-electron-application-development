41170a94310b60d5302d760b901112fd2f81ba89
yinm <yinm.cminor9@gmail.com>
2017-08-14 17:42:08 +0900

    add filePath property

--- markdown_editor/src/main/createFileManager.js
+++ markdown_editor/src/main/createFileManager.js
@@ -1,9 +1,14 @@
 import fs from "fs";
 
 class FileManager {
+  constructor() {
+    this.filePath = "";
+  }
+
   saveFile(filePath, text) {
     return new Promise((resolve) => {
       fs.writeFileSync(filePath, text);
+      this.filePath = filePath;
       resolve();
     });
   }
@@ -11,6 +16,7 @@ class FileManager {
   readFile(filePath) {
     return new Promise((resolve) => {
       const text = fs.readFileSync(filePath, "utf8");
+      this.filePath = filePath;
       resolve(text);
     });
   }
