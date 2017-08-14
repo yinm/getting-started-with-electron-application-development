ad40cddd64b7e6a564d62e1fcabb7d9927788c74
yinm <yinm.cminor9@gmail.com>
2017-08-14 16:34:56 +0900

    add save as new file dialog

---
+++ markdown_editor/src/main/showSaveAsNewFileDialog.js
@@ -0,0 +1,25 @@
+import { dialog } from "electron";
+
+function showSaveAsNewFileDialog() {
+  return new Promise((resolve, reject) => {
+    const file = dialog.showSaveDialog(
+      {
+        title: "save",
+        filters: [{
+          name: "markdown file",
+          extensions: [
+            "md"
+          ]
+        }]
+      }
+    );
+
+    if (file) {
+      resolve(file);
+    } else {
+      reject();
+    }
+  });
+}
+
+export default showSaveAsNewFileDialog;
